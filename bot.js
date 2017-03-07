b47aed579617d7cb9b457c75ec82afe6debec5fc
ryvolum <ryvolum@microsoft.com>
2017-03-07 14:47:51 -0800

    Explicitly created dialogs, bound buttons to actions using beginDialogAction)

--- bot.js
+++ bot.js
@@ -8,9 +8,9 @@ var builder = require('botbuilder');
 // Setup Restify Server
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
-  
+
 // Create chat bot
 var connector = new builder.ChatConnector({
     appId: process.env.MICROSOFT_APP_ID,
@@ -21,83 +21,139 @@ var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
 //=========================================================
-// Bots Dialogs
+// Helpers
 //=========================================================
 
-var menu = [
-    {
-        "title" : "Menu m", 
-        "submenu": [
-           { "title": "Menu m - 1" },
-           { "title": "Menu m - 2" },
-           { "title": "Menu m - 3" }
-        ]
-    },
-    { 
-        "title" : "Menu e", 
-        "submenu": [
-           { "title": "Menu e - 1" },
-           { "title": "Menu e - 2" },
-           { "title": "Menu e - 3" }
-        ]
-    },
-    { 
-        "title" : "Menu u", 
-        "submenu": [
-           { "title": "Menu u - 1" },
-           { "title": "Menu u - 2" },
-           { "title": "Menu u - 3" }
-        ]
-    },
-    { 
-        "title" : "Menu l", 
-        "submenu": [
-           { "title": "Menu l - 1" },
-           { "title": "Menu l - 2" },
-           { "title": "Menu l - 3" }
-        ]
-    },
-    { 
-        "title" : "Menu t", 
-        "submenu": [
-           { "title": "Menu t - 1" },
-           { "title": "Menu t - 2" },
-           { "title": "Menu t - 3" }
-        ]
-    },
-    { 
-        "title" : "Menu a", 
-        "submenu": [
-           { "title": "Menu a - 1" },
-           { "title": "Menu a - 2" },
-           { "title": "Menu a - 3" }
-        ]
-    }    
-];
+function createHeroCard(session, title, buttonNames) {
+    var buttons = [];
+    buttonNames.forEach(function (buttonName) {
+        buttons.push(
+            builder.CardAction
+                .dialogAction(session, buttonName, null, "Intent " + buttonName)
+        )
+    })
+    return new builder.HeroCard(session)
+        .title(title)
+        .buttons(buttons);
+}
 
-bot.dialog('/', 
+//=========================================================
+// Bot Dialogs
+//=========================================================
+
+bot.dialog('/',
     function (session) {
-        session.beginDialog("/menu");
+        session.beginDialog("/topLevelMenu");
     }
 );
 
-bot.dialog('/menu', 
+bot.dialog('/topLevelMenu',
     function (session, args) {
         var buttons = [];
-        for(var menuid in menu){
-            var menuItem = menu[menuid];
-            buttons.push(builder.CardAction.postBack(session, menuItem.title, menuItem.title));
-        }
+        var card = createHeroCard(session, "Top Level", ["A", "B", "C"]);
 
         var msg = new builder.Message(session)
-                .textFormat(builder.TextFormat.xml)
-                .attachments([
-                    new builder.HeroCard(session)
-                        .title("Menu")
-                        .subtitle("Choose your option")
-                        .buttons(buttons)
-                ]);
-
-        session.send(msg);
+            .textFormat(builder.TextFormat.xml)
+            .attachments([card]);
+        session.endDialog(msg);
     }
 );
+
+bot.dialog('/A',
+    function (session, args) {
+        session.send("In Dialog A");
+        var buttons = [];
+        var card = createHeroCard(session, "Dialog A", ["A1", "A2", "A3"]);
+        var msg = new builder.Message(session)
+            .textFormat(builder.TextFormat.xml)
+            .attachments([card]);
+        session.endDialog(msg);
+    }
+);
+
+bot.dialog('/B',
+    function (session, args) {
+        session.send("In Dialog A");
+        var buttons = [];
+        var card = createHeroCard(session, "Dialog B", ["B1", "B2", "B3"]);
+        var msg = new builder.Message(session)
+            .textFormat(builder.TextFormat.xml)
+            .attachments([card]);
+        session.endDialog(msg);
+    }
+);
+
+bot.dialog('/C',
+    function (session, args) {
+        session.send("In Dialog A");
+        var buttons = [];
+        var card = createHeroCard(session, "Dialog C", ["C1", "C2", "C3"]);
+        var msg = new builder.Message(session)
+            .textFormat(builder.TextFormat.xml)
+            .attachments([card]);
+        session.endDialog(msg);
+    }
+);
+
+bot.dialog('/A1',
+    function (session) {
+        session.endDialog("In dialog A1");
+    });
+
+bot.dialog('/A2',
+    function (session) {
+        session.endDialog("In dialog A2");
+    });
+
+bot.dialog('/A3',
+    function (session) {
+        session.endDialog("In dialog A3");
+    });
+
+bot.dialog('/B1',
+    function (session) {
+        session.endDialog("In dialog B1");
+    });
+
+bot.dialog('/B2',
+    function (session) {
+        session.endDialog("In dialog B2");
+    });
+
+bot.dialog('/B3',
+    function (session) {
+        session.endDialog("In dialog B3");
+    });
+
+bot.dialog('/C1',
+    function (session) {
+        session.endDialog("In dialog C1");
+    });
+
+bot.dialog('/C2',
+    function (session) {
+        session.endDialog("In dialog C2");
+    });
+
+bot.dialog('/C3',
+    function (session) {
+        session.endDialog("In dialog C3");
+    });
+
+// No 'matches' option means that these actions can only be triggered by buttons
+bot.beginDialogAction('A', '/A');
+bot.beginDialogAction('A1', '/A1');
+bot.beginDialogAction('A2', '/A2');
+bot.beginDialogAction('A3', '/A3');
+
+bot.beginDialogAction('B', '/B');
+bot.beginDialogAction('B1', '/B1');
+bot.beginDialogAction('B2', '/B2');
+bot.beginDialogAction('B3', '/B3');
+
+bot.beginDialogAction('C', '/C');
+bot.beginDialogAction('C1', '/C1');
+bot.beginDialogAction('C2', '/C2');
+bot.beginDialogAction('C3', '/C3');
+
+
