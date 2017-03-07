8038bb17d0789862bed2a058d25ce43f67cb0bff
meulta <etienne.margraff@live.fr>
2017-03-07 11:45:08 -0800

    main level menu

--- bot.js
+++ bot.js
@@ -25,32 +25,79 @@ server.post('/api/messages', connector.listen());
 //=========================================================
 
 var menu = [
-    "Menu m",
-    "Menu e",
-    "Menu u",
-    "Menu l",
-    "Menu t",
-    "Menu a"
+    {
+        "title" : "Menu m", 
+        "submenu": [
+           { "title": "Menu m - 1" },
+           { "title": "Menu m - 2" },
+           { "title": "Menu m - 3" }
+        ]
+    },
+    { 
+        "title" : "Menu e", 
+        "submenu": [
+           { "title": "Menu e - 1" },
+           { "title": "Menu e - 2" },
+           { "title": "Menu e - 3" }
+        ]
+    },
+    { 
+        "title" : "Menu u", 
+        "submenu": [
+           { "title": "Menu u - 1" },
+           { "title": "Menu u - 2" },
+           { "title": "Menu u - 3" }
+        ]
+    },
+    { 
+        "title" : "Menu l", 
+        "submenu": [
+           { "title": "Menu l - 1" },
+           { "title": "Menu l - 2" },
+           { "title": "Menu l - 3" }
+        ]
+    },
+    { 
+        "title" : "Menu t", 
+        "submenu": [
+           { "title": "Menu t - 1" },
+           { "title": "Menu t - 2" },
+           { "title": "Menu t - 3" }
+        ]
+    },
+    { 
+        "title" : "Menu a", 
+        "submenu": [
+           { "title": "Menu a - 1" },
+           { "title": "Menu a - 2" },
+           { "title": "Menu a - 3" }
+        ]
+    }    
 ];
 
-bot.dialog('/', function (session) {
-    session.beginDialog("/menu");
-});
-
-bot.dialog('/menu', function (session) {
-    var buttons = [];
-    for(var menuid in menu){
-        buttons.push(builder.CardAction.postBack(session, menu[menuid], menu[menuid]))
+bot.dialog('/', 
+    function (session) {
+        session.beginDialog("/menu");
     }
+);
 
-    var msg = new builder.Message(session)
-            .textFormat(builder.TextFormat.xml)
-            .attachments([
-                new builder.HeroCard(session)
-                    .title("Menu")
-                    .subtitle("Choose your option")
-                    .buttons(buttons)
-            ]);
+bot.dialog('/menu', 
+    function (session, args) {
+        var buttons = [];
+        for(var menuid in menu){
+            var menuItem = menu[menuid];
+            buttons.push(builder.CardAction.postBack(session, menuItem.title, menuItem.title));
+        }
 
-    session.send(msg);
-});
+        var msg = new builder.Message(session)
+                .textFormat(builder.TextFormat.xml)
+                .attachments([
+                    new builder.HeroCard(session)
+                        .title("Menu")
+                        .subtitle("Choose your option")
+                        .buttons(buttons)
+                ]);
+
+        session.send(msg);
+    }
+);
